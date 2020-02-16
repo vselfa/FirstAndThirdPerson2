ff03a47a3ec19eb1e140db4e7d3ecb955db8510c
vselfa <vselfa@ieseljust.com>
2020-02-16 21:18:47 +0100

    Final games:4 scenes + interconnexion between them

--- Assets/Scripts/FPStoneController.cs
+++ Assets/Scripts/FPStoneController.cs
@@ -1,38 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// To manage scenes 
+using UnityEngine.SceneManagement;
 
 public class FPStoneController : MonoBehaviour { // Only ONE class to control all the stones
     
     private Rigidbody rigidBody; 
     private Vector3 offset;    // The distance 
     private bool following = false; 
-      
+    // static: to keep values between updates
+    private static int orderToFollow = 0;
+
     public GameObject target; // The target to follow
     
     void Start()  { 	
-        rigidBody = GetComponent<Rigidbody>(); offset = gameObject.transform.position - target.transform.position;
+        rigidBody = GetComponent<Rigidbody>(); 
+        offset = gameObject.transform.position - target.transform.position;
     }
     // To control the collisions with the First Person object from the OWN stones
     void OnCollisionEnter(Collision other) {
     
+        // Follow an order to change of scene: Red, Blue and Yellow
         if (other.gameObject.tag == "FPCTag") {  // Interacting ONLY with the First Person Controller
-            if (gameObject.tag == "RedStoneTag") { 
-                print("Xoc amb la bola roja!!");   
+           
+           if (gameObject.tag == "RedStoneTag") { 
                 Destroy (gameObject);   
+                if (orderToFollow == 0) orderToFollow = 1;
+                print("Xoc amb la bola roja!! Order: " +  orderToFollow);   
             }
-            if (gameObject.tag == "BlueStoneTag") { following = true;
-                if (!following) { 
-                    print("Xoc amb la bola blava!!");            
-                }
-                else {                  
-                    print("Seguint bola blava!!");                  
-                }
+
+            if (gameObject.tag == "BlueStoneTag") { 
+                following = true;
+                if (orderToFollow == 1) orderToFollow = 2;
+                print("Seguint bola blava!! Order: " +  orderToFollow);  
             }
+
             if (gameObject.tag == "YellowStoneTag") { 
-                print("Xoc amb la bola groga!!"); 
                 rigidBody.AddForce(Vector3.forward * Random.Range(1, 5), ForceMode.Impulse);   
+                if (orderToFollow == 2) orderToFollow = 3;
+                print("Xoc amb la bola groga!! Order: " +  orderToFollow);   
             }
+
             // Static script to control the number of collisions
             ControlCollisions.incNumCollisions(); 
             print("Num. colisions: " + ControlCollisions.getNumCollisions());
@@ -41,7 +50,13 @@ public class FPStoneController : MonoBehaviour { // Only ONE class to control al
     // To update the position of the camera AFTER the update of the sphere
     void LateUpdate() { 
          if (following){  
-            if (gameObject.tag == "BlueStoneTag") {gameObject.transform.position = target.transform.position + offset;}
+            if (gameObject.tag == "BlueStoneTag") {
+                gameObject.transform.position = target.transform.position + offset;
+            }
+        }
+
+        if (orderToFollow == 3) {
+            SceneManager.LoadScene ("ThirdPerson");
         }
     }
 }
--- Assets/Scripts/TPStoneController.cs
+++ Assets/Scripts/TPStoneController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// To manage scenes 
+using UnityEngine.SceneManagement;
 
 public class TPStoneController : MonoBehaviour { // Only ONE class to control all the stones
     private Rigidbody rigidBody; 
@@ -8,38 +10,51 @@ public class TPStoneController : MonoBehaviour { // Only ONE class to control al
     private bool following = false;   
 
     public GameObject target; // The target to follow
+    // static: to keep values between updates
+    private static int orderToFollow = 0;
 
     void Start()  { 	
-        rigidBody = GetComponent<Rigidbody>(); offset = gameObject.transform.position - target.transform.position;
+        rigidBody = GetComponent<Rigidbody>(); 
+        offset = gameObject.transform.position - target.transform.position;
     }
     void OnCollisionEnter(Collision other) {
+        
+        // Follow an order to change of scene: Red, Blue and Yellow
         if (other.gameObject.tag == "TPCTag") {  // Interacting ONLY with the First Person Controller
-            if (gameObject.tag == "RedStoneTag") {
-                 print("Xoc amb la bola roja!!");   
-                 Destroy (gameObject);   
+            if (gameObject.tag == "RedStoneTag") { 
+                Destroy (gameObject);   
+                if (orderToFollow == 0) orderToFollow = 1;
+                print("Xoc amb la bola roja!! Order: " +  orderToFollow);   
             }
-            if (gameObject.tag == "BlueStoneTag") { following = true;
-                if (!following) { 
-                    print("Xoc amb la bola blava!!");            
-                }
-                else {                  
-                    print("Seguint bola blava!!");                 
-                }
+
+            if (gameObject.tag == "BlueStoneTag") { 
+                following = true;
+                if (orderToFollow == 1) orderToFollow = 2;
+                print("Seguint bola blava!! Order: " +  orderToFollow);  
             }
-            if (gameObject.tag == "YellowStoneTag") {
-                print("Xoc amb la bola groga!!"); 
+
+            if (gameObject.tag == "YellowStoneTag") { 
                 rigidBody.AddForce(Vector3.forward * Random.Range(1, 5), ForceMode.Impulse);   
+                if (orderToFollow == 2) orderToFollow = 3;
+                print("Xoc amb la bola groga!! Order: " +  orderToFollow);   
             }
 
-            ControlCollisions.incNumCollisions(); print("Num. colisions: " + ControlCollisions.getNumCollisions());
-
+            // Static script to control the number of collisions
+            ControlCollisions.incNumCollisions(); 
+            print("Num. colisions: " + ControlCollisions.getNumCollisions());
         }
     }
-    void LateUpdate() { // To update the position of the camera AFTER the update of the sphere
+
+    // To update the position of the camera AFTER the update of the sphere
+    void LateUpdate() { 
          if (following){  
             if (gameObject.tag == "BlueStoneTag") {
                 gameObject.transform.position = target.transform.position + offset;
             }
+        }
+
+        if (orderToFollow == 3) {
+            SceneManager.LoadScene ("Final");
         }
     }
 }
